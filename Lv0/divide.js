67b3dc4cb15418a223b80a46a26734ae2e0201b8
daehan <daehanl8635@gmail.com>
2024-10-28 18:10:35 +0900

    lv0.몫 구하기

---
+++ Lv0/divide.js
@@ -0,0 +1,9 @@
+function divide(num1, num2) {
+    var answer = 0;
+    answer = parseInt(num1 / num2);
+    return answer;
+}
+
+console.log(divide(5,2));
+console.log(divide(10,3));
+console.log(divide(13,4));
