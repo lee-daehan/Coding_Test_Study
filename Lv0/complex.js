617b546d92763be342ba24e6adda9cb4209ce474
daehan <daehanl8635@gmail.com>
2024-10-28 18:08:13 +0900

    lv0.두 수의 곱

---
+++ Lv0/complex.js
@@ -0,0 +1,9 @@
+function complex(num1, num2) {
+    var answer = 0;
+    answer = num1 * num2;
+    return answer;
+}
+
+console.log(complex(1,3));
+console.log(complex(-1,-3));
+console.log(complex(3,-5));
