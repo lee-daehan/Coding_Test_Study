4a19cff5d3e86123f5790ae421eca651a8c17919
daehan <daehanl8635@gmail.com>
2024-10-28 18:05:13 +0900

    lv0.두 수의 합

---
+++ Lv0/plus.js
@@ -0,0 +1,10 @@
+function plus(num1, num2) {
+    var answer = 0;
+    answer = num1 + num2
+    return answer;
+}
+
+console.log(plus(1,2));
+console.log(plus(-1,-3));
+console.log(plus(-1,5));
+console.log(plus(-11,3));
